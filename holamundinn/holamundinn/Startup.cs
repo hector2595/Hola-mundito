cbed88bc5fa2e90d653adb5cb7674ee773b81c51
Hector <hectormendieta2595@gmail.com>
2017-12-28 08:52:26 -0500

    hola este es una prueba

---
+++ holamundinn/holamundinn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(holamundinn.Startup))]
+namespace holamundinn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
